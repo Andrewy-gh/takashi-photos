4c49d5b5d981290c816e509479213668066a616f
andy <andy.yu617@gmail.com>
2023-08-01 20:29:28 +0000

    error handler send token clear header to clienside

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,32 +1,32 @@
-const logger = require('./logger');
-const jwt = require('jsonwebtoken');
-const AppError = require('./AppError');
+const logger = require("./logger");
+const jwt = require("jsonwebtoken");
+const AppError = require("./AppError");
 
 const requestLogger = (req, res, next) => {
   // prevents logging of user information
-  if (req.path !== '/auth') {
-    logger.info('Method:', req.method);
-    logger.info('Path:  ', req.path);
-    logger.info('Body:  ', req.body);
-    logger.info('---');
+  if (req.path !== "/auth") {
+    logger.info("Method:", req.method);
+    logger.info("Path:  ", req.path);
+    logger.info("Body:  ", req.body);
+    logger.info("---");
   }
   next();
 };
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
-  if (!authHeader?.startsWith('Bearer ')) {
+  if (!authHeader?.startsWith("Bearer ")) {
     // return res.sendStatus(401);
-    throw new AppError(401, 'unauthorized App Error Message');
+    throw new AppError(401, "unauthorized App Error Message");
   }
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
       next(err);
     }
     if (decoded.id !== process.env.ADMIN_ID) {
       // return res.sendStatus(401);
-      throw new AppError(401, 'unauthorized App Error Message');
+      throw new AppError(401, "unauthorized App Error Message");
     }
     next();
   });
@@ -35,23 +35,24 @@ const verifyJWT = (req, res, next) => {
 const errorHandler = (err, req, res, next) => {
   logger.error(err.message);
 
-  if (err.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' });
-  } else if (err.name === 'ValidationError') {
+  if (err.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (err.name === "ValidationError") {
     return res.status(400).json({ error: err.message });
-  } else if (err.name === 'JsonWebTokenError') {
+  } else if (err.name === "JsonWebTokenError") {
     return res.status(401).json({
-      error: 'invalid token',
+      error: "invalid token",
     });
-  } else if (err.name === 'TokenExpiredError') {
+  } else if (err.name === "TokenExpiredError") {
+    res.set("Clear-Token", "true");
     return res.status(401).json({
-      error: 'token expired',
+      error: "token expired",
     });
   } else if (err instanceof AppError) {
     return res.status(err.statusCode).json({ error: err.message });
   }
 
-  return res.status(500).json({ error: 'Something went wrong' });
+  return res.status(500).json({ error: "Something went wrong" });
 };
 
 module.exports = {
