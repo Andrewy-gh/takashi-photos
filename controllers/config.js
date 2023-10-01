f9bba1d6870325d74396de359e3d549442563c81
andy <andy.yu617@gmail.com>
2023-10-01 06:13:48 +0000

    app check middleware

--- controllers/config.js
+++ controllers/config.js
@@ -1,43 +1,30 @@
-const bcrypt = require('bcrypt');
-const Config = require('../models/Config');
-const ImageOrder = require('../models/ImageOrder');
-const User = require('../models/User');
-
-const checkAppSetup = async (req, res) => {
-  const config = await Config.findOne();
-  if (!config) {
-    const newConfig = new Config({
-      adminCreated: false,
-      imageOrderCreated: false,
-    });
-    newConfig.save();
-    return res.status(200).json({ status: 'Initial setup completed' });
-  }
-  return res.status(200).json({ status: 'App has already been initialized' });
-};
+const bcrypt = require("bcrypt");
+const Config = require("../models/Config");
+const ImageOrder = require("../models/ImageOrder");
+const User = require("../models/User");
 
 const checkAdminPresence = async (req, res) => {
   const config = await Config.findOne({});
   if (!config?.adminCreated) {
-    return res.status(200).json({ status: 'admin not present' });
+    return res.status(200).json({ status: "admin not present" });
   }
-  res.status(200).json({ status: 'admin created' });
+  res.status(200).json({ status: "admin created" });
 };
 
 const createAdmin = async (req, res) => {
   const { email, password } = req.body;
   const config = await Config.findOne();
 
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    return res.status(400).json({
-      error: 'email must be unique',
-    });
-  }
-
   if (!config.adminCreated) {
     config.adminCreated = true;
     await config.save();
+  }
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({
+      error: "email must be unique",
+    });
   }
 
   const saltRounds = 10;
@@ -45,33 +32,14 @@ const createAdmin = async (req, res) => {
   const user = new User({
     email,
     passwordHash,
-    role: 'admin',
+    role: "admin",
   });
 
   const savedUser = await user.save();
   res.status(201).json(savedUser);
 };
 
-const createImageOrder = async (req, res) => {
-  const config = await Config.findOne({});
-  console.log('====================================');
-  console.log(config);
-  console.log('====================================');
-  if (!config?.imageOrderCreated) {
-    config.imageOrderCreated = true;
-    await config.save();
-    const imageOrder = new ImageOrder({});
-    await imageOrder.save();
-    res.status(201).json(imageOrder);
-    console.log('====================================');
-    console.log('image order created');
-    console.log('====================================');
-  }
-};
-
 module.exports = {
-  checkAppSetup,
   checkAdminPresence,
   createAdmin,
-  createImageOrder,
 };
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,30 +1,46 @@
-const logger = require('./logger');
-const jwt = require('jsonwebtoken');
-const AppError = require('./AppError');
+const logger = require("./logger");
+const jwt = require("jsonwebtoken");
+const AppError = require("./AppError");
+const Config = require("../models/Config");
+const ImageOrder = require("../models/ImageOrder");
 
 const requestLogger = (req, res, next) => {
   // prevents logging of user information
-  if (req.path !== '/auth') {
-    logger.info('Method:', req.method);
-    logger.info('Path:  ', req.path);
-    logger.info('Body:  ', req.body);
-    logger.info('---');
+  if (req.path !== "/auth") {
+    logger.info("Method:", req.method);
+    logger.info("Path:  ", req.path);
+    logger.info("Body:  ", req.body);
+    logger.info("---");
   }
+  next();
+};
+
+const initializeApp = async (req, res, next) => {
+  const existingConfig = await Config.findOne();
+  if (!existingConfig) {
+    const newConfig = new Config({
+      adminCreated: false,
+    });
+    await newConfig.save();
+    const imageOrder = new ImageOrder({});
+    await imageOrder.save();
+  }
+
   next();
 };
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
-  if (!authHeader?.startsWith('Bearer ')) {
-    throw new AppError(401, 'Unauthorized');
+  if (!authHeader?.startsWith("Bearer ")) {
+    throw new AppError(401, "Unauthorized");
   }
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
       next(err);
     }
     if (decoded.id !== process.env.ADMIN_ID) {
-      throw new AppError(401, 'Unauthorized');
+      throw new AppError(401, "Unauthorized");
     }
     next();
   });
@@ -33,27 +49,28 @@ const verifyJWT = (req, res, next) => {
 const errorHandler = (err, req, res, next) => {
   logger.error(err.message);
 
-  if (err.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' });
-  } else if (err.name === 'ValidationError') {
+  if (err.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (err.name === "ValidationError") {
     return res.status(400).json({ error: err.message });
-  } else if (err.name === 'JsonWebTokenError') {
+  } else if (err.name === "JsonWebTokenError") {
     return res.status(401).json({
-      error: 'invalid token',
+      error: "invalid token",
     });
-  } else if (err.name === 'TokenExpiredError') {
+  } else if (err.name === "TokenExpiredError") {
     return res.status(401).json({
-      error: 'token expired',
+      error: "token expired",
     });
   } else if (err instanceof AppError) {
     return res.sendStatus(err.statusCode).json({ error: err.message });
   }
 
-  return res.status(500).json({ error: 'Something went wrong' });
+  return res.status(500).json({ error: "Something went wrong" });
 };
 
 module.exports = {
   requestLogger,
+  initializeApp,
   verifyJWT,
   errorHandler,
 };
